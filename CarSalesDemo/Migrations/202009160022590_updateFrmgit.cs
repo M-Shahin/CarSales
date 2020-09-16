0649b48f3f1b49b689eba33a4ce583e754ea2513
M-Shahin <shahin.sarker004@gmail.com>
2020-09-16 10:44:51 +1000

    AddMissingFileInViewModel

---
+++ CarSalesDemo/Migrations/202009160022590_updateFrmgit.cs
@@ -0,0 +1,16 @@
+namespace CarSalesDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateFrmgit : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ CarSalesDemo/ViewModel/CarBodyTypeViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CarSalesDemo.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSalesDemo.ViewModel
+{
+    public class CarBodyTypeViewModel
+    {
+        public IEnumerable<BodyType> BodyTypes { get; set; }
+        
+        public Car Car { get; set; }
+
+        public int? Id { get; set; }
+
+        [Required]
+        [Display(Name = "Make")]
+        public string Make { get; set; }
+
+        [Required]
+        [Display(Name = "Model")]
+        public int Model { get; set; }
+
+        [Required]
+        [Display(Name = "Engine")]
+        public string Engine { get; set; }
+
+        [Required]
+        [Display(Name = "Body Type")]
+        public byte BodyTypeId { get; set; }
+
+        //public BodyType BodyType { get; set; }
+
+        [Required]
+        [Display(Name = "Number of Doors")]
+        public int NumberOfDoors { get; set; }
+
+        [Required]
+        [Display(Name = "Number of Wheels")]
+        public int NumberOfWheels { get; set; }
+
+        public CarBodyTypeViewModel()
+        {
+            Id = 0;
+        }
+
+        public CarBodyTypeViewModel(Car car)
+        {
+            Id = car.Id;
+            Make = car.Make;
+            Model = car.Model;
+            Engine = car.Engine;
+            BodyTypeId = car.BodyTypeId;
+            NumberOfDoors = car.NumberOfDoors;
+            NumberOfWheels = car.NumberOfWheels;
+        }
+    }
+}
